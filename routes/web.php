72475a88ef4f2821ead0bbe9da08f8261575bba1
RolandJosephFrando <201810415@gordoncollege.edu.ph>
2021-04-14 23:13:38 +0800

    CRUD

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\postController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,8 +15,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
+
+Auth::routes();
+
+
+
+Route::get('/orders', [App\Http\Controllers\OrderResources::class, 'index'])->name('orders');
+
+Route::get('/ordersProduct', [App\Http\Controllers\OrderProuductResources::class, 'index'])->name('ordersProduct');
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('/posts', postController::Class);
